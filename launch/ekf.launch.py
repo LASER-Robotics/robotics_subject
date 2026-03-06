8945f9da1dc7f0ffb9cf1432d33c6d2efb79f868
G4burieru <joaogabriel3102@gmail.com>
2026-03-06 10:04:48 -0300

    Fixed identation error on ekf

--- launch/ekf.launch.py
+++ launch/ekf.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
                     name='ekf_filter_node',
                     output='screen',
                     parameters=[os.path.join(get_package_share_directory("robotics_subject"), 'params', 'ekf.yaml')],
-                ),
+                )
 
     ld = LaunchDescription()
 
